2da93634e02059d789c06f3e56eb1c011b5054d2
Romulo Martins Pereira <mp.romulo@gmail.com>
2021-11-23 15:04:12 -0300

    Internationalization

---
+++ src/main/java/com/romulomp/restwebapi/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.romulomp.restwebapi;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good-morning-message", null, locale);
+	}
+	
+}
